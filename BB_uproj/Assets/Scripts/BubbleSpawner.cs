04597a9968756f71c6171d32661417d94d8c1fa8
plyncstudios <jim.hunt@plyncstudios.com>
2021-12-09 10:28:10 -0500

    Organizational Changes made
    Organized files and commented on some of the code.

---
+++ BB_uproj/Assets/Scripts/BubbleSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour
+{
+    /*This script contains the spawn points and bubble prefabs and is responsible for randomizing their locations, types and spawning them*/
+
+
+    public GameObject[] bubblePrefabs;          //Public variable that allows for addition of the bubble prefabs in the Unity Editor
+    public Transform[] spawnPoints;             //Public variaible that allows for addition of the spawn points in the Unity Editor
+
+    //Delay times for Bubble Spawns
+    public float minDelay = 0.1f;
+    public float maxDelay = 1f;
+
+    
+    void Start()
+    {
+        StartCoroutine(SpawnBubbles());
+        
+    }
+
+   IEnumerator SpawnBubbles ()
+    {
+        while (true)
+        {
+            //spawn bubbles
+
+            float delay = Random.Range(minDelay, maxDelay);             //Variable for the delay time between when bubbles spawn)
+            yield return new WaitForSeconds(2f);
+
+            int spawnIndex = Random.Range(0, spawnPoints.Length);       //Variable that randomizes which spawn point the bubbles will appear at.
+            int bubbleIndex = Random.Range(0, bubblePrefabs.Length);    //Variable that randomizes which bubble spawn. 
+            GameObject bubblePrefab = bubblePrefabs[bubbleIndex];   
+            Transform spawnPoint = spawnPoints[spawnIndex];
+
+            Instantiate(bubblePrefab, spawnPoint.position, spawnPoint.rotation);        //Adds the randomized bubble at the randomized spawn point.
+            
+        }
+    }
+}
---
+++ BB_uproj/Assets/Scripts/DestroyBubble.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DestroyBubble : MonoBehaviour
+{
+    /*This script contains the elements that destory the bubble when it is touched. I separated it into two separate units, 
+     * I did this because at the time of programming I was struggling to figure out a way to only add the counter when a counting mode bubble was destroyed. I am going to consolitdate*/
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();           //Random Rigidbody that I am still unsure of why it's hear but removing it seems to make the code angry.
+        
+    }
+
+    [Header("Unity Setup")]  
+
+    [SerializeField] private Transform textScore;   //Variable for the texScore 
+
+    [SerializeField] private Rigidbody2D rb;        //Removing this seems to cause the system to be upset. Paul; I am actually finding little reason to keep this in the code. Let me know if I am wrong...
+
+    public ParticleSystem deathParticles;
+
+   
+
+    void OnMouseDown()
+    {
+        ScoreScript.scoreValue += 1;                                                    //Adds to the score value of a counting mode bubble when it is destroyed.
+        Instantiate(deathParticles, transform.position, Quaternion.identity);           //Adds the particle elements to the scene at the point of destruction.
+        Instantiate(textScore, transform.position, Quaternion.identity);                //Adds the text score to the scene at the point of destruction.
+        //Instantiate(textColor, transform.position, Quaternion.identity);
+        Destroy(gameObject);                                                            //Destroys the game object.
+
+        
+
+    }
+}
